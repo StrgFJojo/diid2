7ecf45b3ba23159c4dade3f000d222dd552e64fb
Josephine van Delden <64866819+StrgFJojo@users.noreply.github.com>
2022-11-12 14:08:38 +0100

    mirror input

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ def run(
             total=frame_provider.total_frames,
         )
     ):
+        img = cv2.flip(img, 0)
         # For non-webcam input, skip frames if desired
         if video != "0" and frame_idx % frame_skip != 0:
             continue
